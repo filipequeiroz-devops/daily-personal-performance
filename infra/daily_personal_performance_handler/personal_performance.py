cfe922241f9b96de2e4c796bca653fab1baa3aa7
Filipe <filipe.queiroz.abreu@gmail.com>
2026-06-07 11:27:41 -0300

    adding env variables, fixing the code structure

--- infra/daily_personal_performance_handler/personal_performance.py
+++ infra/daily_personal_performance_handler/personal_performance.py
@@ -42,11 +42,10 @@ def lambda_handler(event, context):
     try:
         req_headers = event.get('headers', {})
         auth_header = req_headers.get('Authorization') or req_headers.get('authorization')
-        
-        user_email = verificar_token(auth_header)
+        user_email  = verificar_token(auth_header)
         
         # Bloqueio de segurança: Permite apenas o meu e-mail
-        MEU_EMAIL_AUTORIZADO = "filipe.queiroz.abreu@gmail.com" 
+        MEU_EMAIL_AUTORIZADO = os.environ.get('MEU_EMAIL_AUTORIZADO')
         
         if not user_email or user_email != MEU_EMAIL_AUTORIZADO:
             return {
